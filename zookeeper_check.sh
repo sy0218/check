4a32d2d35edf991d448a76290120a430e0bd1ee0
root <root@sy0218-control1>
2024-11-18 17:21:57 +0900

    20241118

--- zookeeper_check.sh
+++ zookeeper_check.sh
@@ -6,16 +6,16 @@ if [ "$#" -ne 1 ]; then
         exit 1
 fi
 
-zoo_server_file="/data/work/system_download.txt"
-zoo_array=($(cat ${zoo_server_file} | grep zookeeper_ip | awk -F '|' '{for(i=2; i<=NF; i++) print $i}'))
-len_zoo_array=${#zoo_array[@]}
+system_file="/data/work/system_download.txt"
+zoo_array=($(cat ${system_file} | grep zookeeper_ip | awk -F '|' '{for(i=2; i<=NF; i++) print $i}'))
+len_array=${#zoo_array[@]}
 
 
 zookeeper_working_dir=$1
 zkServer_where=$(find ${zookeeper_working_dir} -type f -name zkServer.sh)
 
 echo $zkServer_where
-for ((i=0; i<len_zoo_array; i++));
+for ((i=0; i<len_array; i++));
 do
         current_ip=${zoo_array[$i]}
         echo "------------zookeeper type ${current_ip}------------"
